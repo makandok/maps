778088a096621b31686fbec4eb75384ba52cd2f7
Zed Coder <makandok@yahoo.com>
2015-05-25 03:06:56 +0200

    changed default font to dejavu from arial regular. using map.zoom_all,

--- zambiamap.py
+++ zambiamap.py
@@ -4,7 +4,7 @@ mapfile = '/usr/share/webmaps/zambia-all-linux.xml'
 map_output = '/var/www/html/maps/map_of_zambia.png'
 m = mapnik.Map(1200, 900)
 mapnik.load_map(m, mapfile)
-
-bbox = mapnik.Envelope(mapnik.Coord(21.7529, -18.271), mapnik.Coord(33.9697, -7.8416))
-m.zoom_to_box(bbox)
+m.zoom_all()
+#bbox = mapnik.Envelope(mapnik.Coord(21.7529, -18.271), mapnik.Coord(33.9697, -7.8416))
+#m.zoom_to_box(bbox)
 mapnik.render_to_file(m, map_output)
